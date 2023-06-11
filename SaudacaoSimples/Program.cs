45cf239d39298b18149c08d4a21745f8fb2343a9
hugoa <hugoa@DESKTOP-0AT38H3>
2023-06-11 13:04:23 -0300

    Adicionar arquivos de projeto.

---
+++ SaudacaoSimples/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Olá, Bem vindo ao programa de saudação");
+Console.WriteLine("Qual o seu nome?");
+string nomeUsuario = Console.ReadLine()!;
+Console.WriteLine("Olá, {0}!", nomeUsuario);
